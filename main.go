5c6acf97e5aec2797611cf70a8d1d10688ccd263
asaudu <adaoraa.91@gmail.com>
2024-07-11 13:31:33 -0500

    can now get event by ID

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"addyCodes.com/RestAPI/db"
 	"addyCodes.com/RestAPI/models"
@@ -18,8 +19,25 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:eventId", getEvent)
 	server.POST("/events", createEvent)
 	server.Run(":8080")
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("eventId"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID."})
+		return
+	}
+
+	event, err := models.GetEventById(dbOperations.DB, eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 func getEvents(context *gin.Context) {
